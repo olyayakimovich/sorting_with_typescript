dc885ff7929505e93f4897203c2369af10b7dba4
Olya Yakimovich <olgayakimovich87@gmail.com>
2019-11-11 08:58:18 +0300

    set up an app and implement basic bubble sort algorythm forarrays

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const array = [100, 5, 0, -10];
+const sorter = new Sorter(array);
+sorter.sort();
+console.log(array);
